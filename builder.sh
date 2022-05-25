603df771bda8f5548a425f27b4030fe0a80d1297
Vasil Sokolov <vasil@afterlogic.com>
2022-05-25 13:07:51 +0300

    version 1.0.3

---
+++ builder.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DIR=$(cd `dirname $0` && pwd)
+TASK="pack"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -t|--task)
+    TASK="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo TASK: "$TASK"
+
+if [ "$TASK" = "pack" ]; then
+	PRODUCT_VERSION=`cat VERSION`
+	
+	echo CREATE ZIP FILE: "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
+	
+	zip -rq ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip * -x .git .github .gitignore builder.sh
+fi
+
+if [ "$TASK" = "upload" ]; then
+	cd ${DIR}
+	
+	PRODUCT_VERSION=`cat VERSION`
+	
+	echo UPLOAD ZIP FILE: "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
+	
+	curl -v --ftp-create-dirs --retry 6 -T ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip -u ${FTP_USER}:${FTP_PASSWORD} ftp://afterlogic.com/
+fi
